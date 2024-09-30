91a87c371d0f7c022ab36a3632de521c8e2bdce4
DBeewms <dbwcontrol05@gmail.com>
2024-09-30 08:15:00 -0600

    BubbleSortExample

---
+++ BubbleSortExample/Burbuja/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Burbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInputNumeros = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa un número:";
+            // 
+            // tbInputNumeros
+            // 
+            this.tbInputNumeros.Location = new System.Drawing.Point(150, 34);
+            this.tbInputNumeros.Name = "tbInputNumeros";
+            this.tbInputNumeros.Size = new System.Drawing.Size(200, 20);
+            this.tbInputNumeros.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(385, 31);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.Location = new System.Drawing.Point(150, 93);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(200, 186);
+            this.lbNumbers.TabIndex = 3;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(385, 93);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 4;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbInputNumeros);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInputNumeros;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Button btnOrdenar;
+    }
+}
+
