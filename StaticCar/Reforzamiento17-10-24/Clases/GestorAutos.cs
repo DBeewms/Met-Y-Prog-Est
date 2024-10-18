ed433748a743211dbc9bae45e7950935feb8639b
DBeewms <dbwcontrol05@gmail.com>
2024-10-17 18:00:00 -0600

    StaticCar

---
+++ StaticCar/Reforzamiento17-10-24/Clases/GestorAutos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reforzamiento17_10_24.Clases
+{
+    internal class GestorAutos
+    {
+        public struct Carro
+        {
+            public string marca;
+            public string modelo;
+            public int anio;
+            public string color;
+
+            public List<string> historialReparaciones;
+        }
+
+        public static Carro InicializarCarro()
+        {
+            Carro carro = new Carro();
+
+            Console.WriteLine("Ingrese la marca del carro:");
+            carro.marca = Console.ReadLine();
+            Console.WriteLine("Ingrese el modelo del carro:");
+            carro.modelo = Console.ReadLine();
+            Console.WriteLine("Ingrese el año del carro:");
+            carro.anio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el color del carro:");
+            carro.color = Console.ReadLine();
+
+            carro.historialReparaciones = new List<string>();
+
+            return carro;
+        }
+
+        public static Carro AgregarReparacion(Carro carro)
+        {
+            Console.WriteLine("Ingrese la reparación realizada:");
+            string reparacion = Console.ReadLine();
+
+            carro.historialReparaciones.Add(reparacion);
+
+            return carro;
+        }
+
+        public static void MostrarCarro(Carro carro)
+        {
+            Console.WriteLine("Marca: " + carro.marca);
+            Console.WriteLine("Modelo: " + carro.modelo);
+            Console.WriteLine("Año: " + carro.anio);
+            Console.WriteLine("Color: " + carro.color);
+
+            Console.WriteLine("Historial de reparaciones:");
+
+            for (int i = 0; i < carro.historialReparaciones.Count; i++)
+            {
+                Console.WriteLine(carro.historialReparaciones[i]);
+            }
+        }
+    }
+}
---
+++ StaticCar/Reforzamiento17-10-24/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reforzamiento17_10_24.Clases;
+using static Reforzamiento17_10_24.Clases.GestorAutos;
+
+namespace Reforzamiento17_10_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro
+            {
+                historialReparaciones = new List<string>()
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Inicializar carro");
+                Console.WriteLine("2. Agregar reparación");
+                Console.WriteLine("3. Mostrar carro");
+                Console.WriteLine("4. Salir");
+                Console.WriteLine("Seleccione una opcion");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        carro = InicializarCarro();
+                        break;
+                    case "2":
+                        carro = AgregarReparacion(carro);
+                        break;
+                    case "3":
+                        MostrarCarro(carro);
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+}
